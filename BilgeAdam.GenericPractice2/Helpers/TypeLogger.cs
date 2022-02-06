98b29c1efc831b84c518fadc8b3e94f927de14e9
Sergen Kahraman <sergen_kahraman_@hotmail.com>
2022-02-06 16:47:53 +0300

    Testler yazıldı

---
+++ BilgeAdam.GenericPractice2/Helpers/TypeLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeAdam.GenericPractice2.Helpers
+{
+    public class TypeLogger
+    {
+        public string GetTypeProperties<T>()
+        {
+            return null;
+        }
+    }
+}
